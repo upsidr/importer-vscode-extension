c4607309d712dbaf3dea0e0ffb98df0a9c520558
Ryota <rytswd@gmail.com>
2021-11-19 16:53:53 +0000

    Add comment to disable linter error

--- src/importer/fold.ts
+++ src/importer/fold.ts
@@ -4,6 +4,7 @@ export function foldImported() {
 
 }
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 class test implements vscode.FoldingRangeProvider {
     onDidChangeFoldingRanges?: vscode.Event<void> | undefined;
     provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
