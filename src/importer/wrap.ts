3f5543a880f50da721a7d9a0d6abaf932a9101e3
Ryota <rytswd@gmail.com>
2022-02-25 23:42:13 -0500

    Ensure line range selection is correctly handled with wrap with exporter

--- src/importer/wrap.ts
+++ src/importer/wrap.ts
@@ -1,19 +1,17 @@
+import { start } from 'repl';
 import * as vscode from 'vscode';
-import { FileType, MarkerType } from './_util';
+import { FileType, snippet } from './_util';
 
-export const wrapWithImporterMarkers =
-    (outputChannel: vscode.OutputChannel) => wrapWithMarkers(outputChannel, "ImporterMarker");
-export const wrapWithExporterMarkers =
-    (outputChannel: vscode.OutputChannel) => wrapWithMarkers(outputChannel, "ExporterMarker");
-
-function wrapWithMarkers(outputChannel: vscode.OutputChannel, markerType: MarkerType) {
+export function wrapWithExporterMarkers(outputChannel: vscode.OutputChannel) {
     const textEditor = vscode.window.activeTextEditor;
     if (textEditor === undefined) { return; }
 
     const timestamp = new Date();
     const startLine = textEditor.selection.start.line,
         endLine = textEditor.selection.end.line,
-        lines = new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine + 1, 0));
+        endChar = textEditor.selection.end.character;
+
+    const lines = getLineRange(startLine, endLine, endChar);
 
     outputChannel.appendLine(`${timestamp}: start ${startLine}, end ${endLine}`);
 
@@ -27,56 +25,18 @@ function wrapWithMarkers(outputChannel: vscode.OutputChannel, markerType: Marker
 
     outputChannel.appendLine(`${timestamp}: ${text}`);
 
-    textEditor.insertSnippet(snippet(fileExtension as FileType, markerType, text), lines);
+    textEditor.insertSnippet(snippet(fileExtension as FileType, "ExporterMarker", text, "Wrap"), lines);
 }
 
-function snippet(fileType: FileType, markerType: MarkerType, wrappedText: string): vscode.SnippetString {
-    // TODO: Some text blocks are hard coded here. We should be able to have 
-    //       more streamlined setup with code reuse. We may actually be able to
-    //       use Importer to pull in some file content when Importer supports
-    //       JavaScript / TypeScript.
+function getLineRange(startLine: number, endLine: number, endChar: number): vscode.Range {
+    if (startLine === endLine) { return new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine + 1, 0)); }
+    if (endLine > startLine && endChar === 0) { return new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine, 0)); }
 
-    const head = (markerType: MarkerType) => {
-        switch (markerType) {
-            case "ImporterMarker":
-                return "import";
-            case "ExporterMarker":
-                return "export";
-        }
-    };
-    const options = (markerType: MarkerType) => {
-        switch (markerType) {
-            case "ImporterMarker":
-                return "from: ${2:URL or /path/to/file}#${3:lineRange or exporterMarker} ";
-            case "ExporterMarker":
-                return "";
-        }
-    };
-
-    // Quite a bit of assumptions being made, and is going to require more
-    // extensive testing to ensure all the markers are correctly created for
-    // any code.
-    let tmpl: string;
-    switch (fileType) {
-        case "yaml":
-        case "yml":
-            const precedingWhitespace = wrappedText.search(/\S|$/);
-            tmpl =
-                `${' '.repeat(precedingWhitespace)}# == ${head(markerType)}: \${1:name} / begin ${options(markerType)}==\n` +
-                `${wrappedText}` +
-                `${' '.repeat(precedingWhitespace)}# == ${head(markerType)}: \${1:name} / end ==\n`;
-            return new vscode.SnippetString(tmpl);
-        case "md":
-            tmpl =
-                `<!-- == ${head(markerType)}: \${1:name} / begin == -->\n` +
-                `${wrappedText}` +
-                `<!-- == ${head(markerType)}: \${1:name} / end == -->\n`;
-            return new vscode.SnippetString(tmpl);
-    }
+    return new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine + 1, 0));
 }
 
 function isRangeValid(startLine: number, endLine: number, textEditor: vscode.TextEditor): boolean {
-    if (startLine === 0 || endLine === 0) { return false; }
+    // if (startLine === 0 && endLine === 0 ) { return false; }
     if (startLine > endLine) { return false; }
 
     return true;
